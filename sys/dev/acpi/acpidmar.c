7ec033cceaefefaa65337c001d0da177072034ee
Owain G. Ainsworth <oga@nicotinebsd.org>
2014-06-18 01:27:40 +0100

    update copyright

--- sys/dev/acpi/acpidmar.c
+++ sys/dev/acpi/acpidmar.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Owain G. Ainsworth <oga@nicotinebsd.org>
+ * Copyright (c) 2013 - 2014 Owain G. Ainsworth <oga@nicotinebsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
